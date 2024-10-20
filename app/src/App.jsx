08504d33ce8f39fd4f5752d6feb819c7af866aa1
victor reuben <130871216+Victorr204@users.noreply.github.com>
2024-10-20 19:17:18 +0100

    changes

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -3,7 +3,7 @@ import Weather from './components/Weather'
 
 const App = () => {
   return (
-    <div className='app md:w-screen sm:grid sm:w-screen sm:justify-center sm:items-center  sm:bg-pink-400 md:bg-yellow-100 md:bg-opacity-50 sm:bg-opacity-50 '>
+    <div className='app'>
       <Weather/>
     </div>
   )
